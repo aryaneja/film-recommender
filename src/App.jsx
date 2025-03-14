f95d5a2deed50eae1db9e52685174cc2a7064408
ariineja <ariineja@gmail.com>
2025-03-14 14:01:48 +0000

    changed interface, and added github actions

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,28 @@ import React, { useState, useEffect } from 'react';
 
 const App = () => {
     const [film, setFilm] = useState('');
+    const [searchResults, setSearchResults] = useState([]);
+    const [userFilmList, setUserFilmList] = useState(() => {
+        const storedFilms = localStorage.getItem('userFilmList');
+        return storedFilms ? JSON.parse(storedFilms) : [];
+    });
+    const [trendingFilms, setTrendingFilms] = useState([]);
     const [filmRecommendations, setFilmRecommendations] = useState([]);
-    const [films, setFilms] = useState([]);
     const [error, setError] = useState(null);
     const [loading, setLoading] = useState(false);
     const [sortConfig, setSortConfig] = useState({ key: null, direction: null });
 
-    // Function to determine color based on vote average
+    useEffect(() => {
+        localStorage.setItem('userFilmList', JSON.stringify(userFilmList));
+    }, [userFilmList]);
+
     const getVoteColor = (voteAverage) => {
         if (voteAverage > 5) {
-            // Green intensity increases as voteAverage approaches 10
             const green = Math.round(((voteAverage - 5) / 5) * 255);
             return `rgb(${255 - green}, 255, ${255 - green})`;
         } else if (voteAverage < 5) {
-
-            // Red intensity increases as voteAverage approaches 0
             const red = Math.round(((5 - voteAverage) / 5) * 255);
-            return `rgb(255, ${255-red}, ${255-red})`;
+            return `rgb(255, ${255 - red}, ${255 - red})`;
         } else {
             return `rgb(255, 255, 255)`;
         }
@@ -34,9 +39,9 @@ const App = () => {
 
     const sortedFilms = () => {
         if (!sortConfig.key) {
-            return [...films];
+            return [...userFilmList];
         }
-        return [...films].sort((a, b) => {
+        return [...userFilmList].sort((a, b) => {
             if (a[sortConfig.key] < b[sortConfig.key]) {
                 return sortConfig.direction === 'ascending' ? -1 : 1;
             }
@@ -48,67 +53,95 @@ const App = () => {
     };
 
     useEffect(() => {
-        async function fetchData() {
-            await addFilm();
-        }
-        fetchData();
-        return;
+        const delayDebounceFn = setTimeout(() => {
+            if (film.trim()) {
+                addFilm();
+            }
+        }, 500);
+        return () => clearTimeout(delayDebounceFn);
     }, [film]);
 
+    useEffect(() => {
+        const fetchTrendingFilms = async () => {
+            try {
+                const response = await fetch('https://api.themoviedb.org/3/trending/movie/day?language=en-US', {
+                    method: 'GET',
+                    headers: {
+                        accept: 'application/json',
+                        Authorization: `Bearer ${import.meta.env.VITE_TMDB_API_KEY}`,
+                    },
+                });
+                if (!response.ok) {
+                    throw new Error('Failed to fetch trending films');
+                }
+                const data = await response.json();
+                setTrendingFilms(data.results.slice(0, 10));
+            } catch (error) {
+                console.error('Error fetching trending films:', error);
+            }
+        };
+
+        fetchTrendingFilms();
+    }, []);
+
+    const addFilmToUserList = (newFilm) => {
+        setUserFilmList(currentList => {
+            return [...currentList, newFilm]
+        });
+    }
     const getFilmDetails = async (filmId) => {
         setLoading(true);
+        let movieDetails;
+        let director;
+        let studio;
         try {
             const response = await fetch(`https://api.themoviedb.org/3/movie/${filmId}?language=en-US`, {
-                method: 'GET',
-                headers: {
-                    accept: 'application/json',
-                    Authorization: `Bearer ${import.meta.env.VITE_TMDB_API_KEY}`,
-                },
-            });
-
+                    method: 'GET',
+                    headers: {
+                        accept: 'application/json',
+                        Authorization: `Bearer ${import.meta.env.VITE_TMDB_API_KEY}`,
+                    },
+                });
             if (!response.ok) {
                 throw new Error(`HTTP error! status: ${response.status}`);
             }
-            const movieDetails = await response.json();
-
-            let director = "Unknown";
+            movieDetails = await response.json();
+            director = "Unknown";
             if (movieDetails.credits?.crew) {
                 const directorCrew = movieDetails.credits.crew.find(
                     (crewMember) => crewMember.job === "Director"
                 );
                 director = directorCrew ? directorCrew.name : "Unknown";
             }
-            let studio = "Unknown";
-            if(movieDetails.production_companies.length > 0){
+            studio = "Unknown";
+            if (movieDetails.production_companies.length > 0) {
                 studio = movieDetails.production_companies[0].name;
-            } 
+            }
 
-            setFilms([
-                ...films,
-                {
-                    title: movieDetails.title,
-                    release_date: movieDetails.release_date,
-                    popularity: movieDetails.popularity,
-                    vote_average: movieDetails.vote_average,
-                    overview: movieDetails.overview,
-                    poster: movieDetails.poster_path
-                        ? `https://image.tmdb.org/t/p/w200${movieDetails.poster_path}`
-                        : null,
-                    director: director,
-                    language: movieDetails.original_language,
-                    studio: studio,
-                },
-            ]);
+            const newFilm = {
+                id: movieDetails.id,
+                title: movieDetails.title,
+                release_date: movieDetails.release_date,
+                popularity: movieDetails.popularity,
+                vote_average: movieDetails.vote_average,
+                overview: movieDetails.overview,
+                poster: movieDetails.poster_path
+                    ? `https://image.tmdb.org/t/p/w200${movieDetails.poster_path}`
+                    : null,
+                director: director,
+                language: movieDetails.original_language,
+                studio: studio,
+            };
+            addFilmToUserList(newFilm)
+
         } catch (e) {
             setError('Failed to fetch movie details. Please try again.');
             console.error("Error fetching movie details: ", e);
-
         } finally {
-            // Set filmRecommendations to empty after processing the details
-            setFilmRecommendations([]);
             setLoading(false);
         }
-    }
+     };
+
 
     const addFilm = async () => {
         if (film.trim()) {
@@ -127,41 +160,73 @@ const App = () => {
                     }
                 );
 
-                if(!response.ok){
+                if (!response.ok) {
                     throw new Error(`HTTP error! status: ${response.status}`);
                 }
                 const data = await response.json();
 
                 if (data.results) {
-                    setFilmRecommendations(data.results);
+                    setSearchResults(data.results);
                 } else {
                     setError('No results found for this film.');
                 }
-
             } catch (e) {
                 setError('Failed to fetch movies. Please try again.');
                 console.error("Error searching movie: ", e);
-            }
-            finally{
+            } finally {
                 setLoading(false);
             }
         }
     };
 
+    const removeFilm = (filmId) => {
+        const updatedFilmList = userFilmList.filter((film) => film.id !== filmId);
+        setUserFilmList(updatedFilmList);
+    };
+
+/* HTML PART */
+
     return (
         <div className="container">
-            <h1 className="header">Film Recommendations</h1>
-            <input className="input" placeholder="Enter a film name" value={film} onChange={(e) => {setFilm(e.target.value)}} />
-             {filmRecommendations.length > 0 && (
+            <h1 className="header">the film recommender tool</h1>
+            <h2>search for a film</h2>
+            <input className="input" placeholder="Enter a film name" value={film} onChange={(e) => setFilm(e.target.value)} />
+
+            {film.trim() && searchResults.length > 0 &&(
                 <ul className="recommendations">
-                    {filmRecommendations.map((item, index) => (
-                        <li key={item.id} onClick={() => {
-                            getFilmDetails(item.id);                           
-                        }}>
-                            {item.title}
-                        </li>
-                    ))}
-                </ul>)}
+                    {searchResults.map((item, index) => {
+                        const releaseYear = item.release_date ? item.release_date.substring(0, 4) : "N/A";
+                        return (
+                            <li key={item.id} onClick={() => getFilmDetails(item.id)}>
+                                {item.title} [{releaseYear}]
+                            </li>
+                        );
+                    })}
+                </ul>
+            )}
+            
+            {trendingFilms.length > 0 && (
+                <div className="trending-films">
+                    <h2>or choose a trending film</h2>
+                    <div className="trending-films-posters">
+                        {trendingFilms.map((item) => (
+                            <div key={item.id} className="trending-film-item">
+                                <img
+                                    src={`https://image.tmdb.org/t/p/w200${item.poster_path}`}
+                                    alt={item.title}
+                                    onClick={() => getFilmDetails(item.id)}
+                                />
+                                <div className="trending-film-info">
+                                    <span>{item.title}</span>
+                                    <span>{item.release_date ? item.release_date.substring(0, 4) : 'N/A'}</span>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            )}
+
+            <h2>your film list</h2>
             {error && <div className="error">{error}</div>}
             <table>
                 <thead>
@@ -175,19 +240,18 @@ const App = () => {
                         <th onClick={() => requestSort('language')}>Language</th>
                         <th onClick={() => requestSort('studio')}>Studio</th>
                         <th>Poster </th>
+                        <th>Remove</th>
                     </tr>
                 </thead>
                 <tbody>
                     {sortedFilms().map((item, index) => (
                         <tr key={index} className="listItem">
-                            <td>{item.title}</td>
+                            <td>
+                                <a href={`https://www.themoviedb.org/movie/${item.id}`} target="_blank" rel="noopener noreferrer">{item.title}</a>
+                            </td>
                             <td>{item.release_date}</td>
                             <td>{item.popularity}</td>
-                            <td
-                                style={
-                                    { backgroundColor: getVoteColor(item.vote_average), color: "black" }
-                                }
-                            >
+                            <td style={{ backgroundColor: getVoteColor(item.vote_average), color: "black" }}>
                                 {item.vote_average}
                             </td>
                             <td>{item.overview}</td>
@@ -196,6 +260,27 @@ const App = () => {
                             <td>{item.studio}</td>
                             <td>
                                 {item.poster && <img src={item.poster} alt={`${item.title} poster`} />}
+                            </td>
+                            <td>
+                                <button
+                                    onClick={() => removeFilm(item.id)}
+                                    style={{
+                                        background: 'none',
+                                        border: 'none',
+                                        cursor: 'pointer',
+                                        padding: 0,
+                                    }}
+                                >
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        viewBox="0 0 24 24"
+                                        fill="red"
+                                        width="24px"
+                                        height="24px"
+                                    >
+                                        <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z" />
+                                    </svg>
+                                </button>
                             </td>
                         </tr>
                     ))}
