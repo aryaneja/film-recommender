3f9ac2bfd56172604c8b17426a1b5d47b3d56c2e
ariineja <ariineja@gmail.com>
2025-04-10 10:36:34 +0000

    using CDK's inbuilt bundler

--- lambda/cdk/cdk_stack.py
+++ lambda/cdk/cdk_stack.py
@@ -1,7 +1,8 @@
 from aws_cdk import (
-    aws_lambda as _lambda,
     Stack,
+    aws_lambda as _lambda,
 )
+from aws_cdk.aws_lambda_python_alpha import PythonFunction
 from constructs import Construct
 import os
 
@@ -9,22 +10,12 @@ class LetterboxdParserStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        lambda_code_path = os.path.join(os.path.dirname(__file__), "../function")
-
-        letterboxd_parser_lambda = _lambda.Function(
+        letterboxd_parser_lambda = PythonFunction(
             self,
             "LetterboxdParserFunction",
+            entry=os.path.join(os.path.dirname(__file__), "../function"),  # path to parser.py + requirements.txt
             runtime=_lambda.Runtime.PYTHON_3_11,
-            handler="parser.lambda_handler",
-            code=_lambda.Code.from_asset(
-                path=lambda_code_path,
-                bundling=_lambda.BundlingOptions(
-                    image=_lambda.Runtime.PYTHON_3_11.bundling_image,
-                    command=[
-                        "bash", "-c",
-                        "pip install -r requirements.txt -t /asset-output && cp -r . /asset-output"
-                    ]
-                )
-            ),
-            function_name="letterboxd_parser",
+            index="parser.py",  # file name
+            handler="lambda_handler",  # function name
+            function_name="letterboxd_parser"
         )
