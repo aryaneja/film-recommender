da34cb4d65a5ec0cea4409d1503da8dfefcf9bfd
ariineja <ariineja@gmail.com>
2025-04-10 10:28:21 +0000

    reintalled AWS CDK

--- .venv/bin/activate.fish
+++ .venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/user/film-recommendor/lambda/cdk/.venv"
+set -gx VIRTUAL_ENV "/home/user/film-recommendor/.venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
