643861085c9f3cbba7f5d7b6d9614fbd8cfcaef0
ariineja <ariineja@gmail.com>
2025-04-14 20:57:49 +0000

    Updated CDK file with API gateway integration

--- lambda/cdk/cdk_stack.py
+++ lambda/cdk/cdk_stack.py
@@ -1,16 +1,18 @@
 from aws_cdk import (
     Stack,
     aws_lambda as _lambda,
+    aws_apigateway as rest_api,
+    RemovalPolicy,
 )
 from aws_cdk.aws_lambda_python_alpha import PythonFunction
 from constructs import Construct
 import os
 
-class LetterboxdParserStack(Stack):
+class LetterboxdStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        letterboxd_parser_lambda = PythonFunction(
+        parser_lambda = PythonFunction(
             self,
             "LetterboxdParserFunction",
             entry=os.path.join(os.path.dirname(__file__), "../function"),  # path to parser.py + requirements.txt
@@ -18,4 +20,18 @@ class LetterboxdParserStack(Stack):
             index="parser.py",  # file name
             handler="lambda_handler",  # function name
             function_name="letterboxd_parser"
+        )
+
+        api = rest_api.LambdaRestApi(
+            self,
+            "LetterboxdParserApi",
+            handler=parser_lambda,
+            rest_api_name="LetterboxdParserApi",
+            deploy_options=rest_api.StageOptions(stage_name="prod"),
+            default_cors_preflight_options=rest_api.CorsOptions(
+                allow_origins=rest_api.Cors.ALL_ORIGINS,
+                allow_methods=rest_api.Cors.ALL_METHODS,
+                
+            ),
+            removal_policy=RemovalPolicy.DESTROY
         )
