aca85016e080b9bd9c84fb3e28323222b2469bf7
ariineja <ariineja@gmail.com>
2025-04-14 21:01:29 +0000

    updated name of letterboxd parser py name

--- lambda/cdk/app.py
+++ lambda/cdk/app.py
@@ -1,12 +1,12 @@
 import aws_cdk as cdk
 import os
-from cdk_stack import LetterboxdParserStack
+from cdk_stack import LetterboxdStack
 
 app = cdk.App()
 
-LetterboxdParserStack(
+LetterboxdStack(
     app,
-    "LetterboxdParserStack",
+    "LetterboxdStack",
     env=cdk.Environment(
         account=os.environ["CDK_DEFAULT_ACCOUNT"],
         region=os.environ["CDK_DEFAULT_REGION"],
